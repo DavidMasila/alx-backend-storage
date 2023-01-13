881f9c2e826760419716edd8e71bcbe9e1531dad
MasilaDavid <davidmasila97@gmail.com>
2023-01-12 16:06:25 -0800

    SQL advanced

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- Write a SQL script that lists all bands with 
+-- Glam rock as their main style, ranked by their longevity
+
+SELECT band_name from metal_bands
+WHERE style like '%glam rock%'
+ORDER BY CHAR_LENGTH(band_name);
