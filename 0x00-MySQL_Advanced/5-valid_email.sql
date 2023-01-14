8f7c8a82d879c138ffca20d7f7aeb2a31be66460
MasilaDavid <davidmasila97@gmail.com>
2023-01-13 16:06:44 -0800

    SQL advanced

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -6,7 +6,8 @@ CREATE TRIGGER reset_email_on_update
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF NEW.email != OLD.email 
-    THEN SET NEW.valid_email = 0;
+    IF NEW.email != OLD.email
+        THEN SET NEW.valid_email = 0;
+    END IF;
 END $$
 DELIMETER ;
